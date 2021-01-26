d15ee2badb3c36a6765fe7a0da2cf91218b207f6
Jesus Oliver <joliver@allot.com>
2021-01-26 17:32:54 +0100

    Updated to use the external library instead of having the code in the app Improved gradle scripts

--- core/src/org/joliver82/jmewireframe/Game.java
+++ core/src/org/joliver82/jmewireframe/Game.java
@@ -7,13 +7,9 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
-import com.jme3.scene.VertexBuffer;
 import com.jme3.scene.shape.Sphere;
-import com.jme3.util.BufferUtils;
 
-import java.nio.FloatBuffer;
-
-import jme3utilities.MyMesh;
+import org.joliver82.jme3utils.wireframe.BarycentricCoordGenerator;
 
 public class Game extends SimpleApplication {
 
@@ -48,58 +44,18 @@ public class Game extends SimpleApplication {
         rootNode.attachChild(geometry1);
 
         // Uses Barycentric coordinates aproach
-        Mesh barySphere0 = setBarycentricCoords(new Sphere(10, 10, 1));
+        Mesh barySphere0 = BarycentricCoordGenerator.setBarycentricCoords(new Sphere(10, 10, 1));
         Geometry geometry2 = new Geometry("S2", barySphere0);
         geometry2.setLocalTranslation(1.5f, 1.5f, 0);
         geometry2.setMaterial(matWireframeBarycentric);
         rootNode.attachChild(geometry2);
 
         // Uses Barycentric coordinates aproach
-        Mesh barySphere1 = setBarycentricCoords(new Sphere(10, 10, 1));
+        Mesh barySphere1 = BarycentricCoordGenerator.setBarycentricCoords(new Sphere(10, 10, 1));
         Geometry geometry3 = new Geometry("S3", barySphere1);
         geometry3.setLocalTranslation(1.5f, -1.5f, 0);
         geometry3.setMaterial(matWireframeBarycentricNotCulling);
         rootNode.attachChild(geometry3);
     }
 
-    /**
-     * Calculates barycentric coordinates and stores them in the normal buffer
-     * so we don't need to mess with inner jme3 stuff
-     *
-     * The mesh needs to be in triangle mode, oherwise nothing is done
-     *
-     * @param mesh
-     */
-    private Mesh setBarycentricCoords(Mesh mesh)
-    {
-        Mesh outMesh=mesh;
-
-        if(mesh.getMode()==Mesh.Mode.Triangles)
-        {
-            outMesh=MyMesh.expand(mesh);
-
-            FloatBuffer normalBuffer = BufferUtils.createFloatBuffer(outMesh.getVertexCount()*3);
-            outMesh.setBuffer(VertexBuffer.Type.Normal, 3, normalBuffer);
-
-            for (int triIndex = 0; triIndex < outMesh.getTriangleCount(); ++triIndex) {
-                //vertex1 barycoord
-                normalBuffer.put(1);
-                normalBuffer.put(0);
-                normalBuffer.put(0);
-
-                //vertex2 barycoord
-                normalBuffer.put(0);
-                normalBuffer.put(1);
-                normalBuffer.put(0);
-
-                //vertex2 barycoord
-                normalBuffer.put(0);
-                normalBuffer.put(0);
-                normalBuffer.put(1);
-            }
-            normalBuffer.flip();
-        }
-
-        return outMesh;
-    }
 }
